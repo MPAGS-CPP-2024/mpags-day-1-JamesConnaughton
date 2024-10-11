8190d269f33d5274846b564301834132f3485fd0
James Connaughton <james.connaughton@warwick.ac.uk>
2024-10-11 11:07:48 +0000

    second exercise (part 1 ex. 2)

--- mpags-cipher.cpp
+++ mpags-cipher.cpp
@@ -5,5 +5,17 @@ int main(){
     /* This is a 
     Multiline comment*/
 
-    std::cout << "Hello World!\n";
+    int a{5};
+    std::cout << "a is " << a << std::endl;
+    a = 7;
+    std::cout << "a is " << a << std::endl;
+
+    double b{3.6};
+    std::cout << "b is " << b << std::endl;
+    b = 4.8;
+    std::cout << "b is " << b << std::endl;
+
+    // int c = a + b; This DOESN't give the compiler error 
+    int c{a + b};
+    std::cout << "c is " << c << std::endl;
 }
