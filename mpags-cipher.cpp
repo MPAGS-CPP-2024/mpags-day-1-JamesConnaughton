8e599e8354007b42476e9df6abd3cb702ad4f8d1
James Connaughton <james.connaughton@warwick.ac.uk>
2024-10-11 14:16:57 +0000

    Transliterate user input (ex.5)

--- mpags-cipher.cpp
+++ mpags-cipher.cpp
@@ -2,16 +2,57 @@
 #include <string>
 
 int main(){
-    const int a{5};
-    const double b{4.3};
-    std::cout << a * b << std::endl;
+    char in_char{'x'};
+    std::string out_str{""};
 
-    std::cout << a / b << std::endl;
+    // Take each letter from user input
+    while(std::cin >> in_char){
+        
+        // Convert to upper case
+        if (std::isalpha(in_char)){
+            out_str += std::toupper(in_char);
+            continue;
+        }
 
-    const int c{2};
-    std::cout << a / c << std::endl;
+        // Change numbers to words
+        switch (in_char){
+            case '1':
+                out_str += "ONE";
+                break;
+            case '2':
+                out_str += "TWO";
+                break;
+            case '3':
+                out_str += "THREE";
+                break;
+            case '4':
+                out_str += "FOUR";
+                break;
+            case '5':
+                out_str += "FIVE";
+                break;
+            case '6':
+                out_str += "SIX";
+                break;
+            case '7':
+                out_str += "SEVEN";
+                break;
+            case '8':
+                out_str += "EIGHT";
+                break;
+            case '9':
+                out_str += "NINE";
+                break;
+            case '0':
+                out_str += "ZERO";
+                break;
+            default:
+                // Do nothing
+                break;
+        }
+        // None alphanumeric characters left
+        // Don't add them to the string (removed)
 
-    std::string my_string{"Hey!"};
-    char l = my_string[2];
-    std::cout << l << std::endl;
+    }
+    std::cout << out_str << std::endl;
 }
